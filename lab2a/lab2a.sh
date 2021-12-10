ca597195c5c74a74e9855e90dba621f23f1943b2
Mila Hose <mila.hose@gmail.com>
2021-12-10 05:12:04 -0800

    Add lab 2a shell script

---
+++ lab2a/lab2a.sh
@@ -0,0 +1,10 @@
+curl -X GET http://riak:8098/riak?buckets=true
+
+curl http://riak:8098/riak/branches/1/_,_,_
+curl http://riak:8098/riak/branches/2/movies,_,_
+curl http://riak:8098/riak/branches/3/_,_,_
+curl http://riak:8098/riak/branches/4/movies,_,_
+curl http://riak:8098/riak/branches/5/_,_,_
+curl http://riak:8098/riak/branches/6/movies,_,_
+
+curl http://riak:8098/riak/images/gloryroad.jpeg 
